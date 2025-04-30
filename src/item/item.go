847f871d75e4a158929913a4c8e2d117772072cb
l.luzhkov <l.luzhkov@yadro.com>
2025-04-30 12:25:32 +0300

    add Weapon and Armor

--- src/item/item.go
+++ src/item/item.go
@@ -1,6 +1,9 @@
 package item
 
-import "dungeonWhiteBox/src/money"
+import (
+	"dungeonWhiteBox/src/dice"
+	"dungeonWhiteBox/src/money"
+)
 
 type Item struct {
 	Name        string
@@ -9,4 +12,17 @@ type Item struct {
 	IsBroken    bool
 	Tags        []string
 	Description string
+}
+
+type Weapon struct {
+	Base       Item
+	Damage     dice.DicePool
+	Range      uint8
+	IsTwoHand  bool
+	IsUsePrist bool
+}
+
+type Armor struct {
+	Base Item
+	Ac   uint8
 }
